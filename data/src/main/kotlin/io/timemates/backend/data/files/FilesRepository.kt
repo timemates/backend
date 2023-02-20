f0491235ae7b0e60b4f67dd48055c7cf3d7d3d01
y9slavabogu <y9vad9@gmail.com>
2023-02-20 21:27:07 +0100

    Files data layer implementation (#62)
    * refactor&feature: FilesRepository implementation with PostgresqlFilesDataSource and LocalFilesDataSource
    
    * refactor:removed ImageSize

--- data/src/main/kotlin/io/timemates/backend/data/files/FilesRepository.kt
+++ data/src/main/kotlin/io/timemates/backend/data/files/FilesRepository.kt
@@ -1,4 +1,27 @@
 package io.timemates.backend.data.files
 
-class FilesRepository {
+import io.timemates.backend.data.files.datasource.LocalFilesDataSource
+import io.timemates.backend.data.files.datasource.PostgresqlFilesDataSource
+import io.timemates.backend.files.types.File
+import io.timemates.backend.files.types.value.FileId
+import java.io.InputStream
+import io.timemates.backend.files.repositories.FilesRepository as FilesRepositoryContract
+
+
+class FilesRepository(
+    private val localFilesDataSource: LocalFilesDataSource,
+    private val postgresqlFilesDataSource: PostgresqlFilesDataSource
+) : FilesRepositoryContract {
+    override suspend fun save(fileId: FileId, input: InputStream) {
+        localFilesDataSource.save(fileId.string, LocalFilesDataSource.FileType.IMAGE, input)
+    }
+
+    override suspend fun retrieve(file: File): InputStream? {
+        return localFilesDataSource.retrieve(file.fileId.string, LocalFilesDataSource.FileType.IMAGE)
+    }
+
+    override suspend fun remove(fileId: FileId) {
+        localFilesDataSource.remove(fileId.string, LocalFilesDataSource.FileType.IMAGE)
+    }
+
 }
