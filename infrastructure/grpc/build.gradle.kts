879b706bcec2f87ceabb7dbc06188e9e740797bb
Vadim Yaroschuk <y9vad9@gmail.com>
2023-04-11 00:15:57 +0300

    #87: grpc build fails (#88)
    * #87: fix grpc build
    
    * #87: fix grpc build

--- infrastructure/grpc/build.gradle.kts
+++ infrastructure/grpc/build.gradle.kts
@@ -8,7 +8,10 @@ plugins {
 dependencies {
     implementation(libs.grpc.kotlin.stub)
     implementation(libs.grpc.protobuf)
-    implementation(libs.google.protobuf)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.protobuf.kotlin)
+    implementation(libs.protobuf.java)
+    implementation(libs.kotlinx.coroutines)
 }
 
 protobuf {
