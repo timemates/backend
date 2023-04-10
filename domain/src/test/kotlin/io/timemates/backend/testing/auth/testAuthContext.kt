127578f13843fec24c1c0698de60e650efa7d0e2
y9vad9 <y9vad9@gmail.com>
2023-04-10 21:18:51 +0300

    #77: `testAuthContext`

---
+++ domain/src/test/kotlin/io/timemates/backend/testing/auth/testAuthContext.kt
@@ -0,0 +1,22 @@
+package io.timemates.backend.testing.auth
+
+import com.timemates.backend.validation.createOrThrow
+import io.timemates.backend.features.authorization.Authorized
+import io.timemates.backend.features.authorization.AuthorizedContext
+import io.timemates.backend.features.authorization.Scope
+import io.timemates.backend.features.authorization.types.AuthorizedId
+import io.timemates.backend.users.types.value.UserId
+
+/**
+ * Authorization scope for text purposes. Only usage of it is to provide [AuthorizedContext] of given scope [T].
+ */
+inline fun <T : Scope> testAuthContext(
+    userId: UserId = UserId.createOrThrow(0),
+    block: context(AuthorizedContext<T>) () -> Unit,
+) {
+    val context = object : AuthorizedContext<T> {
+        override val authorization: Authorized = Authorized(authorizedId = AuthorizedId(userId.long))
+    }
+
+    return context.run(block)
+}
