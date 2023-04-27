241c821bec3b768ff74bc44ec7d617cd6f0083d1
y9vad9 <y9vad9@gmail.com>
2023-04-27 17:48:20 +0200

    fixed missing implementation

--- features/validation/src/test/kotlin/io/timemates/backend/validation/testValidationScope.kt
+++ features/validation/src/test/kotlin/io/timemates/backend/validation/testValidationScope.kt
@@ -1,9 +1,22 @@
 package io.timemates.backend.validation
 
 import com.timemates.backend.validation.ValidationScope
+import com.timemates.backend.validation.withValidation
 
+/**
+ * A function for testing to pass / fail test where [ValidationScope]
+ * is needed.
+ *
+ * @param block A block of code to be executed within the validation scope.
+ *
+ * @throws AssertionError if the validation fails.
+ */
+@Throws(AssertionError::class)
 fun testValidationScope(block: context(ValidationScope) () -> Unit) {
-    val scope = ValidationScope { message ->
-        throw AssertionError("Validation failed: $message.")
-    }
+    withValidation(
+        handler = {
+            message -> throw AssertionError("Validation failed: $message")
+        },
+        block = block,
+    )
 }
