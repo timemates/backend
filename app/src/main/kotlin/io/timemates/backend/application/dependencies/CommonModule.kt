3e90c76833b538396c15df471421f4cd306298cb
y9slavabogu <y9slavabogu@gmail.com>
2023-06-05 22:30:10 +0200

    #13(4+3): Optimize koin dependencies declarations

--- app/src/main/kotlin/io/timemates/backend/application/dependencies/CommonModule.kt
+++ app/src/main/kotlin/io/timemates/backend/application/dependencies/CommonModule.kt
@@ -2,7 +2,6 @@ package io.timemates.backend.application.dependencies
 
 import com.timemates.backend.time.SystemTimeProvider
 import com.timemates.backend.time.TimeProvider
-import com.timemates.random.RandomProvider
 import com.timemates.random.SecureRandomProvider
 import io.timemates.backend.application.dependencies.configuration.MailerConfiguration
 import io.timemates.backend.common.repositories.EmailsRepository
@@ -11,23 +10,19 @@ import io.timemates.backend.data.common.repositories.SMTPEmailsRepository
 import io.timemates.backend.mailer.SMTPMailer
 import io.timemates.backend.services.timers.GrpcTimersMapper
 import kotlinx.serialization.json.Json
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import java.time.ZoneId
 
 val CommonModule = module {
-    single {
-        GrpcTimersMapper()
-    }
+    singleOf(::GrpcTimersMapper)
     single<TimeProvider> {
         SystemTimeProvider(ZoneId.of("UTC"))
     }
-    single<RandomProvider> {
-        SecureRandomProvider()
-    }
+    singleOf(::SecureRandomProvider)
     single<EmailsRepository> {
-        val configuration = get<MailerConfiguration>()
 
-        when (configuration) {
+        when (val configuration = get<MailerConfiguration>()) {
             is MailerConfiguration.MailerSend -> {
                 MailerSendEmailsRepository(configuration.configuration)
             }
