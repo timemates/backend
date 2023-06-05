dea95edaa207d8fdb29ea0f3d6143327118d8852
y9slavabogu <y9slavabogu@gmail.com>
2023-06-05 22:25:16 +0200

    #13(4+3): Optimize koin dependencies declarations

--- app/src/main/kotlin/io/timemates/backend/application/dependencies/UsersModule.kt
+++ app/src/main/kotlin/io/timemates/backend/application/dependencies/UsersModule.kt
@@ -4,34 +4,20 @@ import io.timemates.backend.data.users.PostgresqlUsersRepository
 import io.timemates.backend.data.users.UserEntitiesMapper
 import io.timemates.backend.data.users.datasource.CachedUsersDataSource
 import io.timemates.backend.data.users.datasource.PostgresqlUsersDataSource
-import io.timemates.backend.users.repositories.UsersRepository
 import io.timemates.backend.users.usecases.EditUserUseCase
 import io.timemates.backend.users.usecases.GetUsersUseCase
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val UsersModule = module {
-    single {
-        PostgresqlUsersDataSource(get())
-    }
+    singleOf(::PostgresqlUsersDataSource)
     single {
         CachedUsersDataSource(100)
     }
-    single {
-        UserEntitiesMapper()
-    }
-    single<UsersRepository> {
-        PostgresqlUsersRepository(
-            postgresqlUsers = get(),
-            cachedUsers = get(),
-            mapper = get(),
-        )
-    }
+    singleOf(::UserEntitiesMapper)
+    singleOf(::PostgresqlUsersRepository)
 
     // Use cases
-    single {
-        EditUserUseCase(usersRepository = get())
-    }
-    single {
-        GetUsersUseCase(usersRepository = get())
-    }
+    singleOf(::EditUserUseCase)
+    singleOf(::GetUsersUseCase)
 }
